99150c8f60ffc76a657d146edc9ece469581fa17
Alex Aitken <alex.aitken@shopify.com>
2020-06-03 18:40:57 -0400

    Fill out start_bidding_in on application runner

--- test/support/system/test_support/application_runner.rb
+++ test/support/system/test_support/application_runner.rb
@@ -18,9 +18,26 @@ module TestSupport
       @assertions = assertions
     end
 
+    def start_bidding_in(auction)
+      create_sniper auction.item_id  # similar to starting the application with an auction_id
+      shows_auction_status("StatusForJoining")
+    end
+
     def stop
       # noop rails system test takes care of starting and stopping the rails
       # server
+    end
+
+    private
+
+    def shows_auction_status(status)
+      assertions.assert_equal status, find("css-lookup-for-status").text
+    end
+
+    def create_sniper(item_id)
+      visit 'path/to/create'
+      fill_in 'form_id', with: item_id
+      click_button('button-identifier')
     end
   end
 end
